7fac651c1a41b7f3b5643efe12acf28f48f3bd04
Roberth Silva <roberthvipleiloes@hotmail.com>
2021-03-30 15:24:34 -0300

    add build folder results and fix package.json start script

---
+++ dist/src/controllers/QuestionsController.js
@@ -0,0 +1,204 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __generator = (this && this.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
+    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (_) try {
+            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [op[0] & 2, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+    }
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var connection_1 = __importDefault(require("../database/connection"));
+var QuestionsController = /** @class */ (function () {
+    function QuestionsController() {
+    }
+    /**LIST QUESTIONS */
+    QuestionsController.prototype.index = function (request, response) {
+        return __awaiter(this, void 0, void 0, function () {
+            var param, id, questions, serializedQuestions;
+            return __generator(this, function (_a) {
+                switch (_a.label) {
+                    case 0:
+                        param = request.params;
+                        id = param.id;
+                        if (!(Number(id) > 0)) return [3 /*break*/, 2];
+                        return [4 /*yield*/, connection_1.default('questions')
+                                .where('questions.id', id)
+                                .select([
+                                "questions.id",
+                                "questions.description",
+                                "questions.status",
+                                "questions.game_id"
+                            ])];
+                    case 1:
+                        questions = _a.sent();
+                        return [3 /*break*/, 4];
+                    case 2: return [4 /*yield*/, connection_1.default('questions')
+                            .select([
+                            "questions.id",
+                            "questions.description",
+                            "questions.status",
+                            "questions.game_id",
+                            "questions.created_at",
+                            "questions.updated_at"
+                        ])];
+                    case 3:
+                        questions = _a.sent();
+                        _a.label = 4;
+                    case 4:
+                        serializedQuestions = questions.map(function (question) {
+                            return {
+                                id: question.id,
+                                description: question.description,
+                                status: question.status,
+                                game_id: question.game_id,
+                                created_at: question.created_at,
+                                updated_at: question.updated_at
+                            };
+                        });
+                        return [2 /*return*/, response.json(serializedQuestions)];
+                }
+            });
+        });
+    };
+    QuestionsController.prototype.getPerGameId = function (request, response) {
+        return __awaiter(this, void 0, void 0, function () {
+            var game_id, questions, serializedQuestions;
+            return __generator(this, function (_a) {
+                switch (_a.label) {
+                    case 0:
+                        game_id = request.body.game_id;
+                        return [4 /*yield*/, connection_1.default('questions')
+                                .where('questions.game_id', game_id)
+                                .select([
+                                "questions.id",
+                                "questions.description",
+                                "questions.status",
+                                "questions.game_id"
+                            ])];
+                    case 1:
+                        questions = _a.sent();
+                        serializedQuestions = questions.map(function (question) {
+                            return {
+                                id: question.id,
+                                description: question.decscription,
+                                status: question.status,
+                                game_id: question.game_id,
+                                created_at: question.created_at,
+                                updated_at: question.updated_at
+                            };
+                        });
+                        return [2 /*return*/, response.json(questions)];
+                }
+            });
+        });
+    };
+    /**CREATE A NEW QUESTION */
+    QuestionsController.prototype.create = function (request, response) {
+        return __awaiter(this, void 0, void 0, function () {
+            var _a, description, status, game_id, created_at, updated_at, answers, trx, insertedQuestion, question_id_1, classAnswer, insertedAnswer, answer_id, error_1;
+            return __generator(this, function (_b) {
+                switch (_b.label) {
+                    case 0:
+                        _a = request.body, description = _a.description, status = _a.status, game_id = _a.game_id, created_at = _a.created_at, updated_at = _a.updated_at, answers = _a.answers;
+                        console.log('ola');
+                        console.log(description, status, game_id, created_at, updated_at, answers);
+                        return [4 /*yield*/, connection_1.default.transaction()];
+                    case 1:
+                        trx = _b.sent();
+                        _b.label = 2;
+                    case 2:
+                        _b.trys.push([2, 6, , 8]);
+                        return [4 /*yield*/, trx('questions').insert({
+                                description: description,
+                                status: status,
+                                game_id: game_id,
+                                created_at: created_at,
+                                updated_at: updated_at
+                            })];
+                    case 3:
+                        insertedQuestion = _b.sent();
+                        question_id_1 = insertedQuestion[0];
+                        console.log(question_id_1);
+                        classAnswer = answers.map(function (answerItem) {
+                            return {
+                                description: answerItem.description,
+                                status: answerItem.status,
+                                question_id: question_id_1,
+                                created_at: answerItem.created_at,
+                                updated_at: answerItem.updated_at
+                            };
+                        });
+                        console.log(classAnswer);
+                        return [4 /*yield*/, trx("answers").insert(classAnswer)];
+                    case 4:
+                        insertedAnswer = _b.sent();
+                        answer_id = insertedAnswer[0];
+                        console.log('answer_id', answer_id);
+                        // Como estamos usando o transaction, todas as querys estão apenas esperando o commit para realmente rodarem.
+                        // Com todas as inserções preparadas, podemos fazer o commit() que faz as inserções nas tabelas.
+                        return [4 /*yield*/, trx.commit()];
+                    case 5:
+                        // Como estamos usando o transaction, todas as querys estão apenas esperando o commit para realmente rodarem.
+                        // Com todas as inserções preparadas, podemos fazer o commit() que faz as inserções nas tabelas.
+                        _b.sent();
+                        /**API aqui então, retorna mensagem de sucesso para o usuário */
+                        return [2 /*return*/, response.status(201).send({
+                                id: question_id_1,
+                                description: description,
+                                status: status,
+                                game_id: game_id,
+                                created_at: created_at,
+                                updated_at: updated_at,
+                                answers: answers
+                            })];
+                    case 6:
+                        error_1 = _b.sent();
+                        console.log('nao inseriu');
+                        // desfaz qualquer alteração no banco
+                        return [4 /*yield*/, trx.rollback()];
+                    case 7:
+                        // desfaz qualquer alteração no banco
+                        _b.sent();
+                        return [2 /*return*/, response.status(400).json({
+                                error: "Unexpected error while creating new question"
+                            })];
+                    case 8: return [2 /*return*/];
+                }
+            });
+        });
+    };
+    return QuestionsController;
+}());
+exports.default = QuestionsController;
---
+++ src/controllers/UserTypeController.js
@@ -0,0 +1,230 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __generator = (this && this.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
+    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (_) try {
+            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [op[0] & 2, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+    }
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var connection_1 = __importDefault(require("../database/connection"));
+var crypto_1 = __importDefault(require("crypto"));
+var UserTypeController = /** @class */ (function () {
+    function UserTypeController() {
+    }
+    /**LISTAR USUÁRIOS */
+    UserTypeController.prototype.index = function (request, response) {
+        return __awaiter(this, void 0, void 0, function () {
+            var param, id, users, serializedUsers;
+            return __generator(this, function (_a) {
+                switch (_a.label) {
+                    case 0:
+                        param = request.params;
+                        id = param.id;
+                        if (!(Number(id) > 0)) return [3 /*break*/, 2];
+                        return [4 /*yield*/, connection_1.default('users')
+                                .where('users.id', id)
+                                .select([
+                                "users.id",
+                                "users.login",
+                                "users.pass",
+                                "users.status",
+                                "users.created_at",
+                                "users.updated_at"
+                            ])];
+                    case 1:
+                        users = _a.sent();
+                        return [3 /*break*/, 4];
+                    case 2: return [4 /*yield*/, connection_1.default('users')
+                            .select([
+                            "users.id",
+                            "users.login",
+                            "users.pass",
+                            "users.status",
+                            "users.created_at",
+                            "users.updated_at"
+                        ])];
+                    case 3:
+                        users = _a.sent();
+                        _a.label = 4;
+                    case 4:
+                        serializedUsers = users.map(function (user) {
+                            return {
+                                id: user.id,
+                                login: user.login,
+                                pass: user.pass,
+                                status: user.status,
+                                created_at: user.created_at,
+                                updated_at: user.updated_at
+                            };
+                        });
+                        return [2 /*return*/, response.json(users)];
+                }
+            });
+        });
+    };
+    UserTypeController.prototype.logon = function (request, response) {
+        return __awaiter(this, void 0, void 0, function () {
+            var _a, login, pass, hashPass, newPass, users, serializedUser;
+            return __generator(this, function (_b) {
+                switch (_b.label) {
+                    case 0:
+                        _a = request.body, login = _a.login, pass = _a.pass;
+                        hashPass = crypto_1.default.createHmac('sha512', pass);
+                        newPass = hashPass.digest('hex');
+                        return [4 /*yield*/, connection_1.default('users')
+                                .where({
+                                login: login,
+                                pass: newPass,
+                                status: 'ATIVO'
+                            }).select('users.id', 'users.login', 'users.status')];
+                    case 1:
+                        users = _b.sent();
+                        serializedUser = users.map(function (user) {
+                            return {
+                                id: user.id,
+                                login: user.login,
+                                status: user.status
+                            };
+                        });
+                        return [2 /*return*/, response.json(serializedUser)];
+                }
+            });
+        });
+    };
+    /**CRIAR NOVO USUÁRIO */
+    UserTypeController.prototype.create = function (request, response) {
+        return __awaiter(this, void 0, void 0, function () {
+            var _a, login, pass, status, hashPass, newPass, insertedUserIds, user_id, error_1;
+            return __generator(this, function (_b) {
+                switch (_b.label) {
+                    case 0:
+                        _a = request.body, login = _a.login, pass = _a.pass, status = _a.status;
+                        hashPass = crypto_1.default.createHmac('sha512', pass);
+                        newPass = hashPass.digest('hex');
+                        _b.label = 1;
+                    case 1:
+                        _b.trys.push([1, 3, , 4]);
+                        return [4 /*yield*/, connection_1.default('users').insert({
+                                login: login,
+                                pass: newPass,
+                                status: status
+                            })];
+                    case 2:
+                        insertedUserIds = _b.sent();
+                        user_id = insertedUserIds[0];
+                        /**API aqui então, retorna mensagem de sucesso para o usuário */
+                        return [2 /*return*/, response.status(201).send({
+                                id: user_id,
+                                login: login,
+                                pass: pass,
+                                status: status
+                            })];
+                    case 3:
+                        error_1 = _b.sent();
+                        return [2 /*return*/, response.status(400).json({
+                                error: "Unexpected error while creating new user"
+                            })];
+                    case 4: return [2 /*return*/];
+                }
+            });
+        });
+    };
+    /**Atualizar usuario */
+    UserTypeController.prototype.edit = function (request, response) {
+        return __awaiter(this, void 0, void 0, function () {
+            var _a, id, login, status, rowsAffected, error_2;
+            return __generator(this, function (_b) {
+                switch (_b.label) {
+                    case 0:
+                        _a = request.body, id = _a.id, login = _a.login, status = _a.status;
+                        console.log(id, login, status);
+                        _b.label = 1;
+                    case 1:
+                        _b.trys.push([1, 3, , 4]);
+                        return [4 /*yield*/, connection_1.default('users')
+                                .where('users.id', id)
+                                .update({
+                                login: login,
+                                status: status
+                            })];
+                    case 2:
+                        rowsAffected = _b.sent();
+                        /**API aqui então, retorna mensagem de sucesso para o usuário */
+                        return [2 /*return*/, response.status(200).send()];
+                    case 3:
+                        error_2 = _b.sent();
+                        /**Se algum erro acontecer no meio do caminho retorna mensagem de Bad Request */
+                        return [2 /*return*/, response.status(400).json({
+                                error: "Unexpected error while editing user"
+                            })];
+                    case 4: return [2 /*return*/];
+                }
+            });
+        });
+    };
+    /**EXCLUIR usuario */
+    UserTypeController.prototype.delete = function (request, response) {
+        return __awaiter(this, void 0, void 0, function () {
+            var param, id, rowsAffected, error_3;
+            return __generator(this, function (_a) {
+                switch (_a.label) {
+                    case 0:
+                        param = request.params;
+                        id = param.id;
+                        _a.label = 1;
+                    case 1:
+                        _a.trys.push([1, 3, , 4]);
+                        return [4 /*yield*/, connection_1.default('users')
+                                .where('users.id', id)
+                                .del()];
+                    case 2:
+                        rowsAffected = _a.sent();
+                        /**API aqui então, retorna mensagem de sucesso para o usuário */
+                        return [2 /*return*/, response.status(200).send(rowsAffected)];
+                    case 3:
+                        error_3 = _a.sent();
+                        /**Se algum erro acontecer no meio do caminho retorna mensagem de Bad Request */
+                        return [2 /*return*/, response.status(400).json({
+                                error: "Unexpected error while deleting user"
+                            })];
+                    case 4: return [2 /*return*/];
+                }
+            });
+        });
+    };
+    return UserTypeController;
+}());
+exports.default = UserTypeController;
