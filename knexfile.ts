59e62f01a463340e14628fa92904ca104885e5fe
Roberth Silva <roberthvipleiloes@hotmail.com>
2021-04-01 11:21:14 -0300

    add require('ts-node/register') on knexfile.ts

--- knexfile.ts
+++ knexfile.ts
@@ -1,16 +1,18 @@
-//import path from 'path';
+require('ts-node/register');
+
+import path from 'path';
 
 module.exports = {
     development: {
         client: 'sqlite3',
         connection:{
-            //filename: path.resolve(__dirname, 'src', 'database', 'database.sqlite')
+            filename: path.resolve(__dirname, 'src', 'database', 'database.sqlite')
         },
         migrations:{
-            //directory: path.resolve(__dirname,'src','database','migrations')
+            directory: path.resolve(__dirname,'src','database','migrations')
         },
         seeds:{
-            //directory: path.resolve(__dirname,'src','database','seeds')
+            directory: path.resolve(__dirname,'src','database','seeds')
         },
         useNullAsDefault: true,
     },
@@ -32,20 +34,20 @@ module.exports = {
     production: {
         client: 'pg',
         connection: {
-            url: process.env.DATABASE_URL,
-            /*host : 'ec2-18-233-83-165.compute-1.amazonaws.com',
+            //url: process.env.DATABASE_URL,
+            host : 'ec2-18-233-83-165.compute-1.amazonaws.com',
             user : 'lfohpdgzphqmnk',
             password : 'c8210d92f1abb31e690248697d35cbf27a03caa5a743c79d4aea725acc0cfa1a',
             port: 5432,
             database : 'da2n37al7cptee',
-            charset: 'utf8',*/
+            charset: 'utf8',
             ssl: true
         },
         pool: {
             min: 2,
             max: 10
         },
-        migrations: {
+        migrations: {            
             directory: path.resolve(__dirname,'src','database','migrations')
         }
     }  
