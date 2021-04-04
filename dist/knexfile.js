ccf922b6ab815a9ecf91be4995da8f757848a555
Roberth Silva <roberthvipleiloes@hotmail.com>
2021-04-04 10:30:18 -0300

    fix errors

--- dist/knexfile.js
+++ dist/knexfile.js
@@ -39,9 +39,7 @@ module.exports = {
     },
     production: {
         client: "pg",
-        connection: {
-            url: process.env.DATABASE_URL,
-        },
+        connection: process.env.DATABASE_URL,
         pool: {
             min: 2,
             max: 10,
