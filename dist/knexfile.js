89a655bbac9c5d5ece3fd35cc043d4a0b9926645
Roberth Silva <roberthvipleiloes@hotmail.com>
2021-04-04 09:49:45 -0300

    fix errors

--- dist/knexfile.js
+++ dist/knexfile.js
@@ -5,49 +5,60 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 var path_1 = __importDefault(require("path"));
 module.exports = {
-    development: {
-        client: 'pg',
-        connection: {
-            database: 'dbteste',
-            user: 'postgres',
-            password: 'rob!123gi0840',
-            port: 5432,
-            host: 'localhost'
-        },
-        migrations: {
-            directory: path_1.default.resolve(__dirname, 'src', 'database', 'migrations')
-        },
-        seeds: {
-            directory: path_1.default.resolve(__dirname, 'src', 'database', 'seeds')
-        },
-        useNullAsDefault: true,
+  development: {
+    client: "pg",
+    connection: {
+      database: "dbteste",
+      user: "postgres",
+      password: "rob!123gi0840",
+      port: 5432,
+      host: "localhost",
     },
-    staging: {
-        client: 'postgresql',
-        connection: {
-            database: 'my_db',
-            user: 'username',
-            password: 'password'
-        },
-        pool: {
-            min: 2,
-            max: 10
-        },
-        migrations: {
-            tableName: 'knex_migrations'
-        }
+    migrations: {
+      directory: path_1.default.resolve(
+        __dirname,
+        "src",
+        "database",
+        "migrations"
+      ),
     },
-    production: {
-        client: 'pg',
-        connection: {
-            url: process.env.DATABASE_URL
-        },
-        pool: {
-            min: 2,
-            max: 10
-        },
-        migrations: {
-            directory: path_1.default.resolve(__dirname, 'dist', 'database', 'migrations')
-        }
-    }
+    seeds: {
+      directory: path_1.default.resolve(__dirname, "src", "database", "seeds"),
+    },
+    useNullAsDefault: true,
+  },
+  staging: {
+    client: "postgresql",
+    connection: {
+      database: "my_db",
+      user: "username",
+      password: "password",
+    },
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      tableName: "knex_migrations",
+    },
+  },
+  production: {
+    client: "pg",
+    connection: {
+      url: process.env.DATABASE_URL,
+    },
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      directory: path_1.default.resolve(
+        __dirname,
+        "dist",
+        "src",
+        "database",
+        "migrations"
+      ),
+    },
+  },
 };
