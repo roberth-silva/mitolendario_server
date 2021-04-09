da4658be4aa6ebb9d0f026aa51e582c6cf0e3fed
Roberth Silva <roberthvipleiloes@hotmail.com>
2021-04-08 21:28:11 -0300

    adding GameController and fix questions insert

--- dist/src/database/connection.js
+++ dist/src/database/connection.js
@@ -5,8 +5,8 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 var knex_1 = __importDefault(require("knex"));
 var knexfile = require('../../knexfile');
-console.log(process.env.NODE_ENV);
-console.log(process.env.DATABASE_URL);
+console.log("NODE_ENV: ", process.env.NODE_ENV);
+console.log("DATABASE_URL", process.env.DATABASE_URL);
 var env = process.env.NODE_ENV || 'development';
 var configOptions = knexfile[env];
 var connection = knex_1.default(configOptions);
